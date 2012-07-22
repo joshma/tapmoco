93092a00b49c5fd29024bc0f2454a9977b670b35
Joshua Ma <me@joshma.com>
2012-07-21 22:06:14 -0700

    update

--- main/mobile.py
+++ main/mobile.py
@@ -1,17 +1,22 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
-
-count = 0
+from django.contrib.auth.models import User
+from django.core.exceptions import DoesNotExist
 
 
 @csrf_exempt
-def status(request, userid=0, loc=0):
-    global count
+def status(request, username=None, loc=0):
+    try:
+        user = User.objects.get(username=username)
+    except DoesNotExist:
+        return HttpResponseBadRequest('invalid username!')
+    profile = user.get_profile()
     if request.method == 'POST':
-        count += 1
-    m = count % 2
+        profile.at_desk = not profile.at_desk
+        profile.save()
+    m = 1 if profile.at_desk else 0
     return HttpResponse(m)
 
 
 def tabs(request, userid=0, loc=0):
-    return HttpResponse('google.com')
+    return HttpResponse('http://google.com')
--- main/models.py
+++ main/models.py
@@ -7,6 +7,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User)
     url = models.CharField(max_length=1000, default="www.tapmo.co")
     secret = models.CharField(max_length=20, default="")
+    at_desk = models.BooleanField(default=False)
 
 
 def create_user_profile(sender, instance, created, **kwargs):
