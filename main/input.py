486d750f44f3edcc3b5cc3621d310e4d21ade564
Joshua Ma <me@joshma.com>
2012-07-21 18:52:40 -0700

    POST updates, GET displays

--- main/input.py
+++ main/input.py
@@ -1,12 +1,11 @@
-from django.views.decorators.http import require_POST
 from django.http import HttpResponse
 
 count = 0
 
 
-@require_POST
 def android(request, userid=0, loc=0):
     global count
-    count += 1
+    if request.POST:
+        count += 1
     m = count % 2
     return HttpResponse(m)
