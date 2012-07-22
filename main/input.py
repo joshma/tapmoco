c177a2cce5ccc37a923d94a7b5c5b916a15f3e6a
Joshua Ma <me@joshma.com>
2012-07-21 17:44:32 -0700

    hit count

--- main/input.py
+++ main/input.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+count = 0
+
 
 def android(request, userid=0, loc=0):
-    return HttpResponse('userid=%s, loc=%s' % (userid, loc))
+    global count
+    count += 1
+    return HttpResponse('userid=%s, loc=%s, count=%d' % (userid, loc, count))
